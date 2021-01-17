f61ef23cc78527fdeb96492036d68da585c2ef8e
Samuel Kogler <samuel.kogler@gmail.com>
2021-01-17 17:03:13 +0100

    Clean up merger script and add data from comp.graphics for testing.

--- src/test/scripts/functions/transform/input/20news_untokenized/NewsgroupToCSVMerger.groovy
+++ src/test/scripts/functions/transform/input/20news_untokenized/NewsgroupToCSVMerger.groovy
@@ -1,31 +1,29 @@
-@groovy.lang.GrabConfig(systemClassLoader = true)
+@GrabConfig(systemClassLoader = true)
 @Grab(group = "org.apache.commons", module = "commons-email", version = "1.5")
 @Grab(group = "com.opencsv", module = "opencsv", version = "5.3")
+
 import org.apache.commons.mail.util.*
+import javax.mail.Session
+import javax.mail.internet.MimeMessage
 import com.opencsv.CSVWriter
-import javax.activation.DataSource;
-import javax.mail.Session;
-import javax.mail.internet.MimeMessage;
 
-CSVWriter writer = new CSVWriter(new FileWriter("yourfile.csv"));
-
-dia = new File("/systemds/src/test/scripts/functions/transform/input/20news_untokenized/20news-18828/sci.crypt")
+CSVWriter writer = new CSVWriter(new FileWriter(args[1]));
+dia = new File(args[0])
+if (!dia.isDirectory()) {
+    println "ERROR: Given file path ${args[0]} is not a directory!"
+    System.exit(1)
+}
+println "Reading newsgroup files from ${args[0]}"
+numRows = 0
 dia.eachFile {
-    print(it.getName())
-    println(it)
     final Session session = Session.getDefaultInstance(new Properties());
     final MimeMessage message = MimeMessageUtils.createMimeMessage(session, it);
     final MimeMessageParser mimeMessageParser = new MimeMessageParser(message);
     mimeMessageParser.parse();
-    println mimeMessageParser.getSubject()
-
-    print("----------------------------------\n")
-
-    // feed in your array (or convert your data to an array)
     String[] entries = [it.getName(), mimeMessageParser.getPlainContent()]
-
     writer.writeNext(entries);
+    numRows += 1
 }
-
 writer.close();
+println "Wrote ${numRows} rows to ${args[1]}"
 
